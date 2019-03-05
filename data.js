246164eef72210c87531579d6ca23cf8eb18da9f
Shajan Jacob <shajanjp@gmail.com>
2019-03-05 16:44:49 +0530

    adds data.json file

---
+++ data.js
@@ -0,0 +1,9 @@
+[
+  {
+    "title": "Amazon Echo Dot",
+    "url": "https://www.amazon.in/All-new-Echo-Dot-3rd-Gen/dp/B0792KTHKK/ref=sr_1_1?ie=UTF8&qid=1551784393&sr=8-1&keywords=google+home+mini",
+    "price": {
+      "lessThan": 4000
+    }
+  }
+]
